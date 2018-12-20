cf06f66334f8b37fa773e78573e0dd7999eb71d9
Legendary <Legendary-tony>
2018-12-21 00:44:54 +0100

    fixing css

--- resources/views/cpanel/cpanel.blade.php
+++ resources/views/cpanel/cpanel.blade.php
@@ -6,12 +6,12 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon.jpg') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.jpg">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>36zero | cpanel</title>
     <!-- CSS -->
-    <link href="{{ asset('admin/assets/vendors/material-icons/material-icons.css') }}" rel="stylesheet" type="text/css">
-    <link href="{{ asset('admin/assets/vendors/mono-social-icons/monosocialiconsfont.css') }}" rel="stylesheet" type="text/css">
+    <link href="admin/assets/vendors/material-icons/material-icons.css" rel="stylesheet" type="text/css">
+    <link href="admin/assets/vendors/mono-social-icons/monosocialiconsfont.css" rel="stylesheet" type="text/css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.6.4/sweetalert2.css" rel="stylesheet" type="text/css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" type="text/css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mediaelement/4.1.3/mediaelementplayer.min.css" rel="stylesheet" type="text/css">
@@ -20,7 +20,7 @@
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,400i,500,700" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600" rel="stylesheet" type="text/css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href="{{ asset('admin/assets/css/style.css') }}" rel="stylesheet" type="text/css">
+    <link href="admin/assets/css/style.css" rel="stylesheet" type="text/css">
     <!-- Head Libs -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
 </head>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -19,7 +19,7 @@
 					var disqus_config = function () {
 					this.page.url = '{{ Request::url() }}';  // Replace PAGE_URL with your page's canonical URL variable
 					this.page.identifier = '{{ $posts->id }}'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-					};
+				};
 
 					(function() { // DON'T EDIT BELOW THIS LINE
 						var d = document, s = d.createElement('script');
@@ -27,13 +27,13 @@
 						s.setAttribute('data-timestamp', +new Date());
 						(d.head || d.body).appendChild(s);
 					})();
-					</script>
-{{-- <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript> --}}
+				</script>
+				{{-- <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript> --}}
 
 
-</div>
-</div>
-</div>
+			</div>
+		</div>
+	</div>
 </section>
 @include('partials.related')
 
